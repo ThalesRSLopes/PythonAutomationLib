b44c837897150de2ce545f72e54d7e8ea2dc6bbc
Thales Lopes <thalesrslopes@gmail.com>
2024-08-14 22:44:29 -0300

    Implementado a classe de envio de e-mails

--- send_email.py
+++ send_email.py
@@ -2,13 +2,14 @@ import os
 from email.message import EmailMessage
 import ssl
 import smtplib
+import keyring
 
 
 class SendSmtpEmail:
 
-    def __init__(self, email_sender, email_password):
+    def __init__(self, email_sender, email_credential_name):
         self.email_sender = email_sender
-        self.email_password = email_password
+        self.email_password = keyring.get_password(email_credential_name,email_sender)
         self.context = ssl.create_default_context()
 
     def send_email(self, email_receiver, subject, body):
@@ -17,3 +18,11 @@ class SendSmtpEmail:
         em['To'] = email_receiver
         em['Subject'] = subject
         em.set_content(body)
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=self.context) as smtpSender:
+            smtpSender.login(self.email_sender, self.email_password)
+            smtpSender.sendmail(self.email_sender, email_receiver, em.as_string())
+
+
+mail_sender = SendSmtpEmail("thalesrslopes@gmail.com","thalesrslopesgmail")
+mail_sender.send_email("thalesrslopes@gmail.com","e-mail teste", "Testando o envio de e-mails com Python")
